dd4dbddd27630b95b3f3cf2b4a270cd649b0e420
Frederick Zhang <frederick888@tsundere.moe>
2017-01-22 23:21:15 +1100

    more tests, better output

--- rust-test.sh
+++ rust-test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 apt-get -y install curl vim git libssh2-1-dev libssl-dev
 
 apt-get -y install apt-transport-https ca-certificates software-properties-common
@@ -8,17 +10,25 @@ add-apt-repository -y "deb https://apt.dockerproject.org/repo/ debian-$(lsb_rele
 apt-get update
 apt-get -y install docker-engine
 
+echo "### INSTALLING cargo-clone ###"
 cargo install cargo-clone
 
 rust_test_template() {
+    echo "### TESTING $1 ###"
     cd /apps
     cargo clone "$1"
     cd "$1"
-    cargo build --target x86_64-pc-windows-gnu
+    env $2 cargo build --target x86_64-pc-windows-gnu
 }
 
 # WINAPI-BUILD
 rust_test_template winapi-build
+
+# LIBZ-SYS
+rust_test_template libz-sys
+
+# OPENSSL-SYS
+#rust_test_template openssl-sys "OPENSSL_DIR=/usr/x86_64-w64-mingw32"
 
 # LIBSSH2-SYS
 rust_test_template libssh2-sys
