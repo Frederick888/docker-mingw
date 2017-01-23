7bb0b916d3fe34d7b3f848791cf7efa10b7fdf03
Frederick Zhang <frederick888@tsundere.moe>
2017-01-24 02:07:54 +1100

    add scripts and patches to /builds

--- scripts/boost_1_63_0.sh
+++ scripts/boost_1_63_0.sh
@@ -4,11 +4,11 @@ cd /builds
 curl -O -J -L https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.gz/download
 tar xf boost_1_63_0.tar.gz
 cd /builds/boost_1_63_0
-patch -Np0 -i /apps/patches/boost/boost-mingw.patch
+patch -Np0 -i /builds/patches/boost/boost-mingw.patch
 cd /builds/boost_1_63_0/libs/serialization
-patch -p1 -i /apps/patches/boost/42.patch
+patch -p1 -i /builds/patches/boost/42.patch
 cd /builds/boost_1_63_0
-cp /apps/patches/boost/user-config.jam ./
+cp /builds/patches/boost/user-config.jam ./
 ./bootstrap.sh --with-toolset=gcc --with-python=/usr/bin/python2
 ./b2 -d+2 -q target-os=windows variant=release threading=multi threadapi=win32 link=static \
         runtime-link=shared --prefix=/usr/x86_64-w64-mingw32 --user-config=user-config.jam --without-python \
--- scripts/libxml2-2.9.4.sh
+++ scripts/libxml2-2.9.4.sh
@@ -4,8 +4,8 @@ cd /builds
 curl -O -J ftp://xmlsoft.org/libxml2/libxml2-2.9.4.tar.gz
 tar xf libxml2-2.9.4.tar.gz
 cd /builds/libxml2-2.9.4
-patch -Np0 -i /apps/patches/libxml2/mingw32-libxml2-static-build-compile-fix.patch
-patch -Np1 -i /apps/patches/libxml2/libxml2-no-test.patch
+patch -Np0 -i /builds/patches/libxml2/mingw32-libxml2-static-build-compile-fix.patch
+patch -Np1 -i /builds/patches/libxml2/libxml2-no-test.patch
 sed -i "s| doc example | |g" Makefile.am
 autoreconf -vfi
 LIBS="-lz" LDFLAGS="-L/usr/x86_64-w64-mingw32/lib" ./configure --without-python --host=x86_64-w64-mingw32 --prefix=/usr/x86_64-w64-mingw32
